ac4acf322ae10dfe0f855b02df988814a748900f
charmjinmean <charmjinmean@gmail.com>
2025-03-14 19:38:13 +0900

    Login 페이지 오류 해결, 250314, 19:38

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,17 +1,5 @@
-import Login from "@/components/Auth/Login";
-import { Metadata } from "next";
+import { redirect } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: "로그인 - LIF",
-  description: "Life, Intelligence, Future 로그인 페이지"
-};
-
-const LoginPage = () => {
-  return (
-    <>
-      <Login />
-    </>
-  );
-};
-
-export default LoginPage; 
+export default function LoginPage() {
+  redirect('/auth/login');
+} 
